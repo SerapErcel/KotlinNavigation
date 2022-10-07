db42b1cc3d986a99ac962bb59990e4ee2d800739
Serap <ercelserap@gmail.com>
2022-10-07 22:27:45 +0300

    Update onBack

--- app/src/main/java/com/serapercel/kotlinnavigation/SayfaYFragment.kt
+++ app/src/main/java/com/serapercel/kotlinnavigation/SayfaYFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import com.serapercel.kotlinnavigation.databinding.FragmentSayfaYBinding
 
 class SayfaYFragment : Fragment() {
     private lateinit var binding: FragmentSayfaYBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentSayfaYBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigate(R.id.sayfaYGitAnasayfa)
+                }
+
+            })
     }
 
 }
